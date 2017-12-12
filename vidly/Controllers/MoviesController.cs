b5b943b501d2c5269dfe20c7c0dbd84c0a2fc3cd
vanessapr.85 <vanessapr.85@gmail.com>
2017-12-12 15:59:25 -0500

    added model and controller for movies

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}
