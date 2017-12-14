baabc8a88943017b66cfc2a7dad4dee8a510203c
Vanessa Milagros <vanessapr.85@gmail.com>
2017-12-14 10:24:36 -0500

    add customer api

---
+++ vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using vidly.Dtos;
+using vidly.Models;
+
+namespace vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
